5c759c9e59586f5b572959fd3287a9a09ade97ce
Youyi Fong <youyifong@gmail.com>
2022-11-13 10:07:37 -0800

    change normalize99 back

--- cellpose/transforms.py
+++ cellpose/transforms.py
@@ -185,7 +185,7 @@ def make_tiles(imgi, bsize=224, augment=False, tile_overlap=0.1):
         
     return IMG, ysub, xsub, Ly, Lx
 
-def normalize99(Y, lower=0,upper=100):
+def normalize99(Y, lower=1,upper=99):
     """ normalize image so 0.0 is 1st percentile and 1.0 is 99th percentile """
     X = Y.copy()
     x01 = np.percentile(X, lower)
